26c4a5766495dcc724f78104006ca191a8b89ca7
nefzina <nefzi.amani@gmail.com>
2023-06-27 18:18:58 +0200

    product CRUD and authorization middleware

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,12 @@
-CREATE TABLE item (
+CREATE TABLE products (
   id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
+  title varchar(255) NOT NULL,
+  type varchar(255) NOT NULL,
+  color varchar(255) NOT NULL,
+  description varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+INSERT INTO products (title, type, color, description) 
+VALUES 
+("monitor", "monitors", "black", "27 pouces"), 
+("computer", "electronics","grey", "hp-md-523654");
---
+++ backend/src/services/auth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  try {
+    const authorizationHeader = req.get("Authorization");
+    if (authorizationHeader == null) {
+      throw Error("Authorization header is missing");
+    }
+    const [type, token] = authorizationHeader.split(" ");
+    if (type !== "Bearer") {
+      throw Error("Authorization header has not the 'Bearer' type");
+    }
+    req.payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    next();
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(401);
+  }
+};
+
+module.exports = {
+  verifyToken,
+};
