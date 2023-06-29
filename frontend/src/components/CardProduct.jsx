4413a07604763a200ffb89cea8a038a161a6ce43
Lopes93600 <120326900+Lopes93600@users.noreply.github.com>
2023-06-29 14:18:43 +0200

    update style for responsive card

--- frontend/src/components/CardProduct.jsx
+++ frontend/src/components/CardProduct.jsx
@@ -1,19 +1,24 @@
+import { PropTypes } from "prop-types";
 import image from "../../../backend/public/assets/images/galaxyNote10.jpg";
 
-export default function CardProduct() {
-  //   console.info(product);
+export default function CardProduct({ product }) {
   return (
     <div className="card-product-container">
       <div className="image-container">
         <img src={image} alt="pic tel" />
       </div>
-      <div className="description-container">
-        <h1>produit</h1>
-        <p>description</p>
-      </div>
-      <div className="price-container">
-        <p>price</p>
+      <div className="informations-container">
+        <div className="description-container">
+          <h1>{product.model}</h1>
+          <p>{product.description}</p>
+        </div>
+        <div className="price-container">
+          <p>{product.price} €</p>
+        </div>
       </div>
     </div>
   );
 }
+CardProduct.propTypes = {
+  product: PropTypes.shape().isRequired,
+};
--- frontend/src/pages/AllProducts.jsx
+++ frontend/src/pages/AllProducts.jsx
@@ -4,17 +4,85 @@ import CardProduct from "../components/CardProduct";
 
 export default function AllProducts() {
   const [allProducts, setAllProducts] = useState([]);
+  const [brand, setBrand] = useState("");
+  const [model, setModel] = useState("");
+  const [modelBox, setModelBox] = useState("");
+  const [location, setLocation] = useState("");
   useEffect(() => {
     axios
       .get(`${import.meta.env.VITE_BACKEND_URL}/stock_products`)
       .then((res) => setAllProducts(res.data))
       .catch((err) => console.error(err));
   }, []);
+
   return (
-    <div className="contact">
-      {allProducts.map((product) => (
-        <CardProduct key={product.id} product={product} />
-      ))}
+    <div className="global-container-products">
+      <div className="filter-container">
+        <div className="marque-container">
+          <h2>Marque</h2>
+          <input
+            type="text"
+            placeholder="Marque"
+            value={brand}
+            onChange={(e) => setBrand(e.target.value)}
+          />
+          {allProducts.map((product) => (
+            <div className="checkbox-container">
+              <input type="checkbox" id="brand" name="brand" />
+              {product.brand}
+            </div>
+          ))}
+        </div>
+        <div className="model-container">
+          <h2>Modèle</h2>
+          <input
+            type="text"
+            placeholder="model"
+            value={model}
+            onChange={(e) => setModel(e.target.value)}
+          />
+          {allProducts.map((product) => (
+            <div className="checkbox-container">
+              <input
+                type="checkbox"
+                id="model"
+                name="model"
+                value={product.model}
+                onChange={(e) => setModelBox(e.target.value)}
+              />
+              {product.model}
+            </div>
+          ))}
+        </div>
+        <div className="model-container">
+          <h2>Localisation</h2>
+          <input
+            type="text"
+            placeholder="model"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+          {allProducts.map((product) => (
+            <div className="checkbox-container">
+              <input type="checkbox" id="localisation" name="localisation" />
+              {product.location}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="allproducts-container">
+        {allProducts
+          .filter((e) => {
+            return (
+              e.brand.toLowerCase().includes(brand) &&
+              (e.model.toLowerCase().includes(model) || modelBox) &&
+              e.location.toLowerCase().includes(location)
+            );
+          })
+          .map((product) => (
+            <CardProduct key={product.id} product={product} />
+          ))}
+      </div>
     </div>
   );
 }
