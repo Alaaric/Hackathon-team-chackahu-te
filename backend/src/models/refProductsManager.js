4365c56350bce327623c04f24a0585224a23502d
Alaric HENROT <alaric.henrot@gmail.com>
2023-06-29 11:06:57 +0200

    get all info for calculator

--- backend/src/models/refProductsManager.js
+++ backend/src/models/refProductsManager.js
@@ -5,6 +5,18 @@ class productsManager extends AbstractManager {
     super({ table: "ref_products" });
   }
 
+  getAllRefPhones() {
+    return this.database.query(
+      `select brand, m.name model, screen_size, network, os.name os,
+      RAMs.value ram, s.value storage from ${this.table}
+      inner join brands on brand_id = brands.id
+      inner join models as m on model_id = m.id
+      inner join os on os_id = os.id
+      inner join RAMs on RAM_id = RAMs.id
+      inner join storages s on storage_id = s.id`
+    );
+  }
+
   insert(product) {
     return this.database.query(
       `insert into ${this.table} (brand_id, model_id, os_id, RAM_id, storage_id, image) values (?, ?, ?, ?, ?, ?)`,
