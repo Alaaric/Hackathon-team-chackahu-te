9114e7b2b5b54216cd681de86fe4fe378d29db05
nefzina <nefzi.amani@gmail.com>
2023-06-29 14:34:01 +0200

    css admin lists

---
+++ frontend/src/components/ComputePrice.js
@@ -0,0 +1,28 @@
+export default function ComputePrice() {
+  const state = "Bon etat";
+  const RAM = 4;
+  const RAMValue = [
+    {
+      1: 30,
+      2: 40,
+      3: 54,
+      4: 60,
+      6: 70,
+      8: 80,
+      12: 100,
+      16: 120,
+    },
+  ];
+
+  const storageValue = [
+    {
+      16: 31,
+      32: 45,
+      64: 66,
+      128: 99,
+      256: 160,
+      512: 280,
+      1000: 500,
+    },
+  ];
+}
